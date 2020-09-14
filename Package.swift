4832a2e7d88ba6e6cd40c2a5a3e9e4a6cfab208e
417-72KI <417.72ki@gmail.com>
2020-09-14 14:18:12 +0900

    add ArgumentParser

--- Package.swift
+++ Package.swift
@@ -5,9 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "iOSVersionSupportChecker",
+    platforms: [.macOS(.v10_15)],
+    products: [
+        .executable(name: "ivsc", targets: ["iOSVersionSupportChecker"])
+    ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.3.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
