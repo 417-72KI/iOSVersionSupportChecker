9e6774da7247b3399e24f1b6a2138a117f49a5d8
417-72KI <417.72ki@gmail.com>
2021-12-21 16:20:57 +0900

    update swift tools version

--- Package.swift
+++ Package.swift
@@ -1,21 +1,21 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "iOSVersionSupportChecker",
-    platforms: [.macOS(.v10_15)],
+    platforms: [.macOS(.v11)],
     products: [
         .executable(name: "ivsc", targets: ["iOSVersionSupportChecker"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.3.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.2"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
+        .executableTarget(
             name: "iOSVersionSupportChecker",
             dependencies: [
                 "iOSVersionSupportCheckerCore",
